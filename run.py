9a796466226de685d2c1c99873bf8c66e545eb7f
Deresz <chris.deresz@emc.com>
2017-01-31 11:43:52 -0500

    first commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
 import os
 from app import app
-port = int(os.environ.get("$PORT", 33507))
-app.run(port=port, debug=True)
+port = int(os.environ.get("PORT", 33507))
+app.run(host='0.0.0.0', port=port, debug=True)
